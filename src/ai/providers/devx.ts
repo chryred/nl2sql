da57f312337e27359751a532305c0fd1c125ae7a
173917 <173917@shinsegae.com>
2026-02-20 15:09:57 +0900

    devx 개선

--- src/ai/providers/devx.ts
+++ src/ai/providers/devx.ts
@@ -9,32 +9,22 @@ import { join } from 'path';
 export class DevX implements AIProvider {
   private client: DevXSDK;
   private agentCode: string;
+  private agentCodeMap = new Map<string, string>([
+    ['playground', 'playground'],
+    ['nl2sql_rel_table', 'custom_e81685f388d442548dce9b49ebad037e'],  // 백화점CX팀_자연어기반 연관 테이블 조회
+    ['nl2sql_query', 'custom_170def4091144a45a99eb302458321ed']       // 백화점CX팀_자연어기반 SQL생성기
+  ]);
 
-  constructor(apiKey: string, agentCode?: string) {
+  constructor(apiKey: string, agentKey?: string) {
     this.client = new DevXSDK();
-    this.agentCode = agentCode || 'playground';
-  }
-
-  async generateSQL(prompt: string): Promise<string> {
-    // const cwd = process.cwd();
-    // const filePath = join(cwd, "user_prompt.md");
-    // writeFileSync(filePath, prompt, { encoding: 'utf-8' });
-
-    const response = await this.client.callAgent({
-      agentCode: "custom_170def4091144a45a99eb302458321ed", // 백화점CX팀_자연어기반 SQL생성기
-      query: `${prompt}`,
-    });
-
-    const textBlock = response.data?.answer;
-    console.log("============ textBlock ==============");
-    console.log(textBlock);
-    console.log("==========================");
-    return textBlock;
+    this.agentCode = this.agentCodeMap.get(agentKey || 'playground')!;
   }
 
   async selectTables(prompt: string): Promise<string> {
+    this.agentCode = this.agentCodeMap.get('nl2sql_rel_table')!;
+
     const response = await this.client.callAgent({
-      agentCode: "custom_e81685f388d442548dce9b49ebad037e",  // 백화점CX팀_자연어기반 연관 테이블 조회
+      agentCode: this.agentCode,
       query: `${prompt}`,
     });
 
@@ -45,5 +35,23 @@ export class DevX implements AIProvider {
     const textBlock = response.data?.answer;
     logger.info(`selectTables response: ${textBlock}`);
     return textBlock || '[]';
+  }
+
+  async generateSQL(prompt: string): Promise<string> {
+    // const cwd = process.cwd();
+    // const filePath = join(cwd, "user_prompt.md");
+    // writeFileSync(filePath, prompt, { encoding: 'utf-8' });
+    this.agentCode = this.agentCodeMap.get('nl2sql_query')!;
+
+    const response = await this.client.callAgent({
+      agentCode: this.agentCode, 
+      query: `${prompt}`,
+    });
+
+    const textBlock = response.data?.answer;
+    console.log("============ textBlock ==============");
+    console.log(textBlock);
+    console.log("==========================");
+    return textBlock;
   }
 }
